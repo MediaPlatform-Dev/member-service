a322bb996ae95d00aee5b2007630710149baebb5
Taehyun Kim <k1m743hyun@icloud.com>
2023-05-26 16:34:35 +0900

    k1m743hyun/member-service:1.2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,6 +4,6 @@ USE member_service;
 DROP TABLE IF EXISTS tbl_member CASCADE;
 CREATE TABLE tbl_member (
     member_num INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    member_id VARCHAR(100) UNIQUE,
+    member_id VARCHAR(100) NOT NULL UNIQUE,
     member_name VARCHAR(20)
 );
